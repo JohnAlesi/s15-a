19f8a028efc90ec84276af141fd2df555c9196b3
John Alesi Resurreccion <resurreccionjohnalesi@gmail.com>
2021-08-24 21:29:36 +0800

    added Gradingconv

--- script.js
+++ script.js
@@ -6,26 +6,30 @@ function gradeconv(num){
 	}
 
 	else if (num >= 75){
-		console.log(num + ' your grade is B+')
+		console.log(num + ' your grade is B+');
 	}
 
 	else if (num >= 65){
-		console.log(num + ' your grade is B')
+		console.log(num + ' your grade is B');
 	}
 
 	else if (num >= 55){
-		console.log(num + ' your grade is C+')
+		console.log(num + ' your grade is C+');
 	}
 
 	else if (num >= 50){
-		console.log(num + ' your grade is C')
+		console.log(num + ' your grade is C');
 	}
 
 	else if (num >= 40){
-		console.log(num + ' your grade is D')
+		console.log(num + ' your grade is D');
 	}
 
-	else {
-		console.log(num + ' your grade is F')
+	else if (num >=1) {
+		console.log(num + ' your grade is F');
+	}
+
+	else if (num < 1) {
+		console.log(num + ' is invalid please enter 1-100');
 	}
 }
